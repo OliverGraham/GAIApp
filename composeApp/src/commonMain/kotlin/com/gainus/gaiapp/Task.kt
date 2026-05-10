c92964749b97015448514674d9af164feb12618e
OliverGraham <olivergraham916@gmail.com>
2026-05-10 11:54:51 -0700

    Implement TODO-001

--- composeApp/src/commonMain/kotlin/com/gainus/gaiapp/Task.kt
+++ composeApp/src/commonMain/kotlin/com/gainus/gaiapp/Task.kt
@@ -5,7 +5,7 @@ data class Task(val id: String, val title: String, val isDone: Boolean)
 fun createSampleTasks(): List<Task> {
     return listOf(
         Task("1", "Buy groceries", false),
-        Task("2", "Finish report", true),
-        Task("3", "Call mom", false)
+        Task("2", "Read a book", true),
+        Task("3", "Walk the dog", false)
     )
 }
