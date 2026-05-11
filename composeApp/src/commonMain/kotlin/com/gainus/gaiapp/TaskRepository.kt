049420c3545485082c34d8b6ba0538f37f88b29c
OliverGraham <olivergraham916@gmail.com>
2026-05-10 20:21:46 -0700

    Implement TODO-007

---
+++ composeApp/src/commonMain/kotlin/com/gainus/gaiapp/TaskRepository.kt
@@ -0,0 +1,10 @@
+package com.gainus.gaiapp
+
+import kotlinx.coroutines.flow.Flow
+
+interface TaskRepository {
+    fun getTasks(): Flow<List<Task>>
+    fun addTask(task: Task)
+    fun toggleTask(task: Task)
+    fun deleteTask(task: Task)
+}
